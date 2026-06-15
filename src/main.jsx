5ab2cfc387bc0af1980424239a363a920f392467
tifjasmine <tiffjasmine92@gmail.com>
2026-06-14 21:33:25 -0400

    Mount authenticated application root

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import Root from "./Root.jsx";
 import "./styles.css";
 import "./finishing.css";
+import "./referral-room.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(<Root />);
